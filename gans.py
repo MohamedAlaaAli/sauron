c0e05b241e797ddf147092135dbf0789c8e5b430
MohamedAlaaAli <muhammed.157@gmail.com>
2025-04-19 07:21:13 +0200

    edit num input chans in disc

--- gans.py
+++ gans.py
@@ -146,7 +146,7 @@ class ConvInstanceNormLeakyReLUBlock(nn.Module):
 
 
 class Discriminator(nn.Module):
-    def __init__(self, in_channels=3, features=[64, 128, 256, 512]):
+    def __init__(self, in_channels=1, features=[64, 128, 256, 512]):
         super().__init__()
         self.initial_layer = nn.Sequential(
             nn.Conv2d(
